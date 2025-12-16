093a5792027db557c793b9ceeabf13be841f9428
Hatesh Lakhani <hateshlakhani9@gmail.com>
2025-12-16 11:53:01 +0500

    update UI

--- matrix4.js
+++ matrix4.js
@@ -292,53 +292,6 @@ function multiply_matrix() {
 
 
 
-// Divided Method
-function division_matrix() {
-    // Get values and convert to numbers
-    let value1 = parseFloat(document.getElementById("matrix-1").value) || 0;
-    let value2 = parseFloat(document.getElementById("matrix-2").value) || 0;
-    let value3 = parseFloat(document.getElementById("matrix-3").value) || 0;
-    let value4 = parseFloat(document.getElementById("matrix-4").value) || 0;
-
-    let value5 = parseFloat(document.getElementById("matrix-5").value) || 0;
-    let value6 = parseFloat(document.getElementById("matrix-6").value) || 0;
-    let value7 = parseFloat(document.getElementById("matrix-7").value) || 0;
-    let value8 = parseFloat(document.getElementById("matrix-8").value) || 0;
-
-
-    let value9 = parseFloat(document.getElementById("matrix-9").value) || 0;
-    let value10 = parseFloat(document.getElementById("matrix-10").value) || 0;
-    let value11 = parseFloat(document.getElementById("matrix-11").value) || 0;
-    let value12 = parseFloat(document.getElementById("matrix-12").value) || 0;
-
-    let value13 = parseFloat(document.getElementById("matrix-13").value) || 0;
-    let value14 = parseFloat(document.getElementById("matrix-14").value) || 0;
-    let value15 = parseFloat(document.getElementById("matrix-15").value) || 0;
-    let value16 = parseFloat(document.getElementById("matrix-16").value) || 0;
-
-    // Divide
-    let arr = []
-
-    arr[0] = value1 / value5 / value9 / value13
-    arr[1] = value2 / value6 / value10 / value14
-    arr[2] = value3 / value7 / value11 / value15
-    arr[3] = value4 / value8 / value12 / value16
-
-
-    document.getElementById("matrix-r1").innerText = Math.round(arr[0]) >= 1000 ? "ERR" : Math.round(arr[0]);
-    document.getElementById("matrix-r2").innerText = Math.round(arr[1]) >= 1000 ? "ERR" : Math.round(arr[1]);
-    document.getElementById("matrix-r3").innerText = Math.round(arr[2]) >= 1000 ? "ERR" : Math.round(arr[2]);
-    document.getElementById("matrix-r4").innerText = Math.round(arr[3]) >= 1000 ? "ERR" : Math.round(arr[3]);
-
-    // Console log
-    console.log("Result Matrix Divison:");
-    console.log(arr[0], arr[1]);
-    console.log(arr[2], arr[3]);
-}
-
-
-
-
 function clear_matrix() {
 
     // Matrix box
